7b24659b48f71988714a2f8ac3cef28f1b145f68
pavana kartik <pavanakartikvelagaelti@gmail.com>
2020-11-09 12:32:25 +0530

    added firebase and angular/fire and added firebase config in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+
+    apiKey: "AIzaSyDk__LH3vy-vzq5XjuGBk4dieqItkrWKJ0",
+    authDomain: "oshop-9a691.firebaseapp.com",
+    databaseURL: "https://oshop-9a691.firebaseio.com",
+    projectId: "oshop-9a691",
+    storageBucket: "oshop-9a691.appspot.com",
+    messagingSenderId: "966360287650",
+    appId: "1:966360287650:web:56a30c2573d6fc250e4afb"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase: {
+
+    apiKey: "AIzaSyDk__LH3vy-vzq5XjuGBk4dieqItkrWKJ0",
+    authDomain: "oshop-9a691.firebaseapp.com",
+    databaseURL: "https://oshop-9a691.firebaseio.com",
+    projectId: "oshop-9a691",
+    storageBucket: "oshop-9a691.appspot.com",
+    messagingSenderId: "966360287650",
+    appId: "1:966360287650:web:56a30c2573d6fc250e4afb"
+  }
 };
 
 /*
