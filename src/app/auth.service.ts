7640c1ef3934ade36666702253cc46242ea12e7d
pavana kartik <pavanakartikvelagaelti@gmail.com>
2020-11-11 16:14:32 +0530

    implemented redirect after login and deployed to firebase

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth'
 import * as firebase from 'firebase';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AuthService {
 
   user$: Observable<firebase.default.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private route:ActivatedRoute) {
 
     
     this.user$ = afAuth.authState;
@@ -19,6 +20,9 @@ export class AuthService {
 
   login(){
 
+    let returnUrl= this.route.snapshot.queryParamMap.get('returnUrl') ||'/';
+
+    localStorage.setItem('returnUrl', returnUrl);
     this.afAuth.signInWithRedirect(new firebase.default.auth.GoogleAuthProvider());
 
            
