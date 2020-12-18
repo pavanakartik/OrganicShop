3977d0fb02a649f8a351f4d795ca62042c2644f2
pavana kartik <pavanakartikvelagaelti@gmail.com>
2020-12-18 15:04:01 +0530

    successfully implemented CheckoutModule

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../auth.service';
+import { Component } from '@angular/core';
+import { OrderService } from '../order.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-my-orders',
   templateUrl: './my-orders.component.html',
   styleUrls: ['./my-orders.component.css']
 })
-export class MyOrdersComponent implements OnInit {
+export class MyOrdersComponent  {
 
-  constructor() { }
+  orders$;
+  
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService) { 
 
-  ngOnInit(): void {
+    this.orders$ = authService.user$.pipe(switchMap(u => orderService.getOrdersByUser(u.uid)));
+ 
+
+ 
   }
 
 }
--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,6 +1,8 @@
+
 import { ShoppingCartService } from './shopping-cart.service';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +11,26 @@ export class OrderService {
 
   constructor(private db: AngularFireDatabase,
     private shoppingCartService: ShoppingCartService
-    ) {
-  
+  ) {
+
   }
 
-async placeOrder(order){
-  let result= await this.db.list('/orders').push(order);
-this.shoppingCartService.clearCart();
-  return result;
-}
+  async placeOrder(order) {
+    let result = await this.db.list('/orders').push(order);
+    this.shoppingCartService.clearCart();
+    return result;
+  }
+
+  getOrders() {
+    return this.db.list('/orders').snapshotChanges();
+  }
+
+  getOrdersByUser(userId: string) {
+
+
+    
+    return this.db.list('/orders', ref => ref.orderByChild('userId').equalTo(userId)).snapshotChanges();
+
+  }
 
 }
- 
