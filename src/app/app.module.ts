a229dc2d8a8ea8f89eea29f2de4dcb373b8d1d01
pavana kartik <pavanakartikvelagaelti@gmail.com>
2020-12-18 19:08:30 +0530

    Created Shopping Module and refactored

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,39 +20,28 @@ import { ProductFormComponent } from './admin/components/product-form/product-fo
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { CheckOutComponent } from './check-out/check-out.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
-import { ProductFilterComponent } from './products/product-filter/product-filter.component';
-import { ProductsComponent } from './products/products.component';
-import { ShippingFormComponent } from './shipping-form/shipping-form.component';
-import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+
+import { ProductsComponent } from './shopping/components/products/products.component';
+
+import { ShoppingModule } from './shopping/shopping.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     BsNavbarComponent,
     HomeComponent,
-    ProductsComponent,
-    ShoppingCartComponent,
-    CheckOutComponent,
-    OrderSuccessComponent,
-    MyOrdersComponent,
-    
-    LoginComponent,
-   
-    ProductFilterComponent,
-    
-    ShoppingCartSummaryComponent,
-    ShippingFormComponent
+
+    LoginComponent
+
+
   ],
   imports: [
     BrowserModule,
     SharedModule,
     AdminModule,
+    ShoppingModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
@@ -63,15 +52,8 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
     DataTablesModule,
     RouterModule.forRoot([
 
-      { path: '', component: ProductsComponent},
+      { path: '', component: ProductsComponent },
       { path: 'login', component: LoginComponent },
-    
-      { path: 'products', component: ProductsComponent },
-      { path: 'shopping-cart', component: ShoppingCartComponent },
-
-      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
-      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuard] },
-      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
 
 
 
@@ -104,9 +86,9 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
     ])
   ],
   providers: [
-    
-   
-    ],
+
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
